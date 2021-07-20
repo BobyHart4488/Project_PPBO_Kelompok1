b318b928d9894445e7bd06d1e63044033ee3e776
BobyHart4488 <hartanto20ti@mahasiswa.pcr.ac.id>
2021-07-20 13:55:04 +0700

    Boby : Pembuatan Query Sistem Login dan Query Data

--- src/project_ppbo_kelompok1/Config.java
+++ src/project_ppbo_kelompok1/Config.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project_ppbo_kelompok1;
 
-/**
- *
- * @author Nesya
- */
+import java.sql.*;
+
 public class Config {
-    
+    private static Connection MySQLConfig;
+    public static Connection configDb()throws SQLException{
+        try {
+            String url= "jdbc:mysql://localhost:3306/project_ppbo_kelompok1";
+            String user= "root";
+            String pass = "";
+
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            MySQLConfig= DriverManager.getConnection(url, user, pass);
+        } catch(SQLException e) {
+            System.err.println("Koneksi Gagal " + e.getMessage());
+        }
+        return MySQLConfig;
+    }
 }
+
